586b8d66e9eed212ca5e87e7e97852bb4b50feff
Gabriel <gabrielberarducci@gmail.com>
2023-05-31 20:02:51 +0200

    urls fixed in project.js

--- resources/js/project.js
+++ resources/js/project.js
@@ -23,10 +23,10 @@ function newRandomChild(parent, data, id = 0){ //el parametro id es opcional-
     if(id > 0) i=id-1; //permite de forma opcional elegir el articulo segun su ID. si no lo hace random.
     newChild.innerHTML = `
     <div class="article" id="${data[i].id}">    
-    <img class="project-img" src="/Projects/Project1/resources/images/projects-section/${(i % 6) + 1}.jpg" alt="Project-image">
+    <img class="project-img" src="/resources/images/projects-section/${(i % 6) + 1}.jpg" alt="Project-image">
         <h2>${data[i].title}</h2>
         <p>${data[i].body}</p>
-        <a rel="stylesheet" href="http://127.0.0.1:5500/Projects/Project1/project.html?id=${data[i].id}">Learn More</a>
+        <a rel="stylesheet" href="/project.html?id=${data[i].id}">Learn More</a>
     </div> 
     `;
     parent.appendChild(newChild);
@@ -51,7 +51,7 @@ function newChildById(parent, data){
             <p class="articleSubtitle">${data[id-1].title}</p>
             <p class="date">Completed on ${formattedDate}</p>
         </div>
-        <img src="/Projects/Project1/resources/images/projects-section/${((id-1) % 6) + 1}.jpg"
+        <img src="/resources/images/projects-section/${((id-1) % 6) + 1}.jpg"
         <p>${data[id-1].body}</p>
     </div> 
     `;
