8c9a54aaa51432cd2b1a047b4a3604a6318ffbb1
Gabriel <gabrielberarducci@gmail.com>
2023-06-01 10:14:41 +0200

    carousel updated now working

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,5 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
 
-
-
 // Loading effect
 window.addEventListener('load', function() {
   const loading = document.querySelector('.loading');
@@ -10,9 +8,6 @@ window.addEventListener('load', function() {
   }, 500);
 
 });
-
-
-
 
 
 // Scroll to TOP section
@@ -36,10 +31,6 @@ function scrollToTop() {
   });
 }
 
-
-
-
-
   
 // create the articles for the Projects sections 
 getPosts().then((data) => {
@@ -48,49 +39,3 @@ getPosts().then((data) => {
   newRandomChild(projectsTabsParent,data,2);
   newRandomChild(projectsTabsParent,data,3);
 })
-
-
-
-
-
-
-//function carousel
-
-const carousel = document.querySelector('.carousel');
-const carouselInner = carousel.querySelector('.carousel-inner');
-const carouselItems = Array.from(carouselInner.querySelectorAll('.carousel-item'));
-
-const itemWidth = carouselItems[0].offsetWidth; // Ancho de cada elemento del carrusel
-const numVisibleItems = 3; // Número de elementos visibles a la vez
-
-let currentIndex = 0;
-const intervalTime = 1000; // Intervalo de tiempo en milisegundos
-let slideInterval;
-
-function startSlide() {
-  slideInterval = setInterval(() => {
-    moveToNextSlide();
-  }, intervalTime);
-}
-
-function moveToNextSlide() {
-  currentIndex++;
-
-  if (currentIndex >= carouselItems.length - numVisibleItems + 1) {
-    currentIndex = 0;
-    carouselInner.style.transition = 'none';
-  } else {
-    carouselInner.style.transition = 'transform 3s ease';
-  }
-
-  carouselInner.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-}
-
-function stopSlide() {
-  clearInterval(slideInterval);
-}
-
-carousel.addEventListener('mouseenter', stopSlide);
-carousel.addEventListener('mouseleave', startSlide);
-
-startSlide();
